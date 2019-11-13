9741a3846dc129028fc8f02b0c2949d5afe9c855
Manuel Egger <m8egger@edu.aau.at>
2019-11-13 13:59:11 +0100

    removed more dependencies

--- src/test/java/com/ase/gymjudge/GymjudgeApplicationTests.java
+++ src/test/java/com/ase/gymjudge/GymjudgeApplicationTests.java
@@ -3,7 +3,7 @@ package com.ase.gymjudge;
 import com.ase.gymjudge.entities.PersonDEMO;
 import com.ase.gymjudge.repositories.PersonRepositoryDEMO;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+// import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
