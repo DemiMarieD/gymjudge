3d1c3542e5fb2f1242c67acc99422fb504a022b9
DemiMarieD <demi-marie@outlook.de>
2019-11-18 20:21:22 +0100

    There is now a login. It is checked if the user exists and password is correct. (Password is saved in plain text this needs to change) There are no error messages yet. Once logged in it will only show the competitions created by that user (only works right after login since there is no session token jet).
    Also there is now a start page with login / register

--- src/main/java/com/ase/gymjudge/repositories/AdminRepository.java
+++ src/main/java/com/ase/gymjudge/repositories/AdminRepository.java
@@ -2,7 +2,23 @@ package com.ase.gymjudge.repositories;
 
 import com.ase.gymjudge.entities.Admin;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AdminRepository extends CrudRepository<Admin, Integer> {
+    @Query(value = "select a from Admin a where a.login like ?1")
+    List<Admin> existsAdminByUsername(String username);
+
+    @Query(value = "select a.password from Admin a where a.login like ?1")
+    String getPasswordFromUser(String username);
+
+    @Query(value = "select a.id from Admin a where a.login like ?1")
+    Integer getIdFromUser(String username);
+
+
+
+   /* @Query(value = "select s from Article s where s.author like ?1 and s.title = ?2")
+    List<Article> findByAuthorAndTitle(String author, String title);*/
 }
--- src/main/java/com/ase/gymjudge/repositories/CompetitionRepository.java
+++ src/main/java/com/ase/gymjudge/repositories/CompetitionRepository.java
@@ -1,9 +1,13 @@
 package com.ase.gymjudge.repositories;
 
 import com.ase.gymjudge.entities.Competition;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CompetitionRepository extends CrudRepository<Competition, Integer> {
+import java.util.List;
 
+public interface CompetitionRepository extends CrudRepository<Competition, Integer> {
+    @Query(value = "select c from Competition c where c.adminID like ?1")
+    List<Competition> getAllByAdminID(Integer userId);
 }
 
