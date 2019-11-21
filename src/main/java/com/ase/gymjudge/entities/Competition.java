e882cb1003ed709b91cedf51c60d7c06927d3c92
Manuel Hobisch <mahobisch@edu.aau.at>
2019-11-21 15:52:13 +0100

    added dropdowns for type and status (added enums for each); changed datatype for the dates to date (as asked in the TODO)

--- src/main/java/com/ase/gymjudge/entities/Competition.java
+++ src/main/java/com/ase/gymjudge/entities/Competition.java
@@ -2,52 +2,67 @@ package com.ase.gymjudge.entities;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Set;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class Competition {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @NotBlank(message = "Name is mandatory")
     private String name;
     //todo: try to get from login token
     private Integer adminID;
+
     private String description;
-    //todo: try with date instead of string
-    private String startDate;
-    private String endDate;
-    private String type;
-    private String status;
+
+    @NotNull
+    private Date startDate;
+
+    @NotNull
+    private Date endDate;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status;
 
     //Setter Methods
     public Integer getId() {
         return id;
     }
+
     public Integer getAdminID() {
         return adminID;
     }
-/*
-    @ManyToOne
-    private Admin admins;
-*/
+
     public String getName() {
         return name;
     }
-    public String getType() {
+
+    public Type getType() {
         return type;
     }
-    public String getStartDate() {
+
+    public Date getStartDate() {
         return startDate;
     }
-    public String getEndDate() {
+
+    public Date getEndDate() {
         return endDate;
     }
-    public String getStatus() {
+
+    public Status getStatus() {
         return status;
     }
+
     public String getDescription() {
         return description;
     }
@@ -56,22 +71,28 @@ public class Competition {
     public void setAdminID(Integer adminID) {
         this.adminID = adminID;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
+
+    public void setStartDate(String startDate) throws ParseException {
+        this.startDate = new SimpleDateFormat("yyyy-mm-dd").parse(startDate);
     }
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
+
+    public void setEndDate(String endDate) throws ParseException {
+        this.endDate = new SimpleDateFormat("yyyy-mm-dd").parse(endDate);
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public void setStatus(String status) {
+
+    public void setStatus(Status status) {
         this.status = status;
     }
-    public void setType(String type) {
+
+    public void setType(Type type) {
         this.type = type;
     }
 
