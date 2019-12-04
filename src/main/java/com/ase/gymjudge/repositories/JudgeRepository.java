e6667c7d1153dcbf975aa30415f9f3ccf32aae1f
Дарья Шевкопляс <shevkoplyasdarya23@gmail.com>
2019-12-04 18:03:24 +0200

    add judge-service and upgrade judge-repository

--- src/main/java/com/ase/gymjudge/repositories/JudgeRepository.java
+++ src/main/java/com/ase/gymjudge/repositories/JudgeRepository.java
@@ -2,7 +2,15 @@ package com.ase.gymjudge.repositories;
 
 import com.ase.gymjudge.entities.Category;
 import com.ase.gymjudge.entities.Judge;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface JudgeRepository extends CrudRepository<Judge, Integer> {
+    Judge findByEmail(String username);
+
+    @Query(value = "select j from Judge j")
+    List<Judge> getAllJudges();
 }
