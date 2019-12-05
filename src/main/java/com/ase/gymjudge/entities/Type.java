68b9d994d7166015b8b55ffe14b9df7ffd59a5cf
Дарья Шевкопляс <shevkoplyasdarya23@gmail.com>
2019-12-05 16:17:37 +0200

    updated Judge and Type classes

--- src/main/java/com/ase/gymjudge/entities/Type.java
+++ src/main/java/com/ase/gymjudge/entities/Type.java
@@ -1,16 +1,29 @@
 package com.ase.gymjudge.entities;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+
 public enum Type {
     TURN10("Turn10"),
     STUFENWETTKAMPF("Stufenwettkampf");
 
     private final String displayValue;
+    //
 
     private Type(String displayValue) {
         this.displayValue = displayValue;
     }
+    //private Type(List<Apparatus> apparatuse){
+      //  this.apparatuse = apparatuse;
+    //}
 
     public String getDisplayValue() {
         return displayValue;
     }
+  //  public List<Apparatus> getApparatuse(){
+        //return apparatuse;
+    //}
 }
