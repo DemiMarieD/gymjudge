26740fe80246548add8b4b77f3f1d241d952cff3
DemiMarieD <demi-marie@outlook.de>
2019-12-05 12:29:01 +0100

    Gymnast-view in Competition-overview can collapse.

---
+++ src/main/resources/static/js/competitionOverview.js
@@ -0,0 +1,11 @@
+
+function displayGymnasts() {
+    document.getElementById("GymnastBody").style.display = "block";
+    document.getElementById("dontShow").style.display = "none";
+    document.getElementById("show").style.display = "block";
+}
+function hideGymnasts() {
+    document.getElementById("GymnastBody").style.display = "none";
+    document.getElementById("dontShow").style.display = "block";
+    document.getElementById("show").style.display = "none";
+}
--- src/main/resources/static/js/testJS.js
+++ src/main/resources/static/js/testJS.js
@@ -7,3 +7,5 @@ $(document).ready(function () {
 
     setInterval(again, 3000);
 });
+
+
