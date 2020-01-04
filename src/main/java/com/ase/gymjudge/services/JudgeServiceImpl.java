ff45d1a031fe8d906b9d9af29f6119c02a8796ed
Dosya_Sheva <34021206+DashaShevkoplyas@users.noreply.github.com>
2020-01-04 13:38:17 +0100

    some upgrades for judge serv/repo (#38)
    * some upgrades for judge serv/repo
    
    * add into competition query for getting judge list
    
    * fix error for repo/services
    
    * fix errors for TODO in SecurityConf,JudgeController, upgraded rest files
    
    * travis setup
    
    * fix errors for TODO in SecurityConf,JudgeController, upgraded rest files

--- src/main/java/com/ase/gymjudge/services/JudgeServiceImpl.java
+++ src/main/java/com/ase/gymjudge/services/JudgeServiceImpl.java
@@ -7,17 +7,17 @@ import com.ase.gymjudge.repositories.JudgeRepository;
 import com.ase.gymjudge.repositories.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
+@Service
 public class JudgeServiceImpl implements JudgeService{
     @Autowired
     private JudgeRepository judgeRepository;
     @Autowired
     private RoleRepository roleRepository;
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public Judge findByLogin(String login) {
@@ -31,4 +31,9 @@ public class JudgeServiceImpl implements JudgeService{
         judge.setRole(role);
         judgeRepository.save(judge);
     }
+    @Override
+    public void delete(Judge judge){
+        judgeRepository.deleteById(judge.getJudgeID());
+    }
 }
+//
