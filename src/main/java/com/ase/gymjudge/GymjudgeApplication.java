310420e959fd7c63f12cdc31e4e271cab3586f43
DemiMarieD <demi-marie@outlook.de>
2020-01-19 23:37:41 +0100

    Selenium is set up and a test for the login is implemented

--- src/main/java/com/ase/gymjudge/GymjudgeApplication.java
+++ src/main/java/com/ase/gymjudge/GymjudgeApplication.java
@@ -1,7 +1,9 @@
 package com.ase.gymjudge;
 
+import org.openqa.selenium.WebDriver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GymjudgeApplication {
